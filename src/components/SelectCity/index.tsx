3d2b8ff5a9f7ef6bbd655b7299b9017512032821
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-15 21:46:27 -0300

    :sparkles: Update to typescript

--- src/components/SelectCity/index.tsx
+++ src/components/SelectCity/index.tsx
@@ -2,14 +2,14 @@ import './styles.css';
 import { useEffect, useState } from 'react';
 
 import { Input } from '../Input';
-import { getCityByNameService } from '../../services/getCityByNameService';
+import { getCityByNameService, CityProps } from '../../services/getCityByNameService';
 
 export function SelectCity({ onSelect }) {
-  const [city, setCity] = useState();
+  const [city, setCity] = useState<CityProps[]>([]);
   const [search, setSearch] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
-  async function getCities(name) {
+  async function getCities(name: string) {
     setIsLoading(true);
 
     const response = await getCityByNameService(name);
@@ -37,9 +37,9 @@ export function SelectCity({ onSelect }) {
 
       <div className='select-list'>
         {
-          city &&
-          <button type="button" key={city.id} onClick={() => onSelect(city)}>
-            <p>{city.name}</p>
+          city[0] &&
+          <button type="button" key={city[0].id} onClick={() => onSelect(city)}>
+            <p>{city[0].name}</p>
           </button>
         }
       </div>
