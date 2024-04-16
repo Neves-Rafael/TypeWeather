c04023060b969683a5d9b2d76cbcaa59891d936e
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-16 13:48:33 -0300

    :sparkles: Feat add types

--- src/services/getCityByNameService.ts
+++ src/services/getCityByNameService.ts
@@ -7,9 +7,21 @@ export interface CityProps {
   longitude: number;
 }
 
+export interface CityAPIResponse{
+  id: string;
+  name: string;
+  sys:{
+    country?: string;
+  }
+  coord:{
+    lat: number;
+    lon: number;
+  }
+}
+
 export async function getCityByNameService(name: string): Promise<CityProps[]> {
   try {
-    const { data } = await api.get(`/weather?q=${name}`);
+    const { data } = await api.get<CityAPIResponse>(`/weather?q=${name}`);
 
     const city = {
       id: data.id,
